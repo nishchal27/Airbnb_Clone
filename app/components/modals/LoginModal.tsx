696eae9181d1da1350a437a52312f793ef4ab2c7
nishchal <singhnishchal27@gmail.com>
2024-01-07 18:09:34 +0530

    github auth functionality commented out

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -93,6 +93,8 @@ const LoginModal = () => {
     </div>
   )
 
+//here removing github auth for some small issues.
+
   const footerContent = (
     <div className="flex flex-col gap-4 mt-3">
       <hr />
@@ -102,12 +104,12 @@ const LoginModal = () => {
         icon={FcGoogle}
         onClick={() => signIn('google')}
       />
-      <Button 
+      {/* <Button 
         outline 
         label="Continue with Github"
         icon={AiFillGithub}
         onClick={() => signIn('github')}
-      />
+      /> */}
       <div className="
       text-neutral-500 text-center mt-4 font-light">
         <p>First time using Airbnb?
--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -95,6 +95,8 @@ const RegisterModal= () => {
     </div>
   )
 
+//here removing github auth for some small issues.
+
   const footerContent = (
     <div className="flex flex-col gap-4 mt-3">
       <hr />
@@ -104,12 +106,12 @@ const RegisterModal= () => {
         icon={FcGoogle}
         onClick={() => signIn('google')} 
       />
-      <Button 
+      {/* <Button 
         outline 
         label="Continue with Github"
         icon={AiFillGithub}
         onClick={() => signIn('github')}
-      />
+      /> */}
       <div 
         className="
           text-neutral-500 
